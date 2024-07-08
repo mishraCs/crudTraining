960deb991d5b3885931ddf290f03277db6d03de0
mishraCs <aakashmishrabba@gmail.com>
2024-07-08 20:27:16 +0530

    now user can add product thier cart on database

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,10 +1,12 @@
 <h1>hello</h1>
 <?php
+include 'Controller/add_product.php';
 if(isset($_GET['SubCategoryId'])){
     $SubCategoryId = $_GET['SubCategoryId'];
-
     $addProduct = new addProduct;
     echo $addProduct->addProductInCart($SubCategoryId);
+    header('location: home.php');
+    echo "<script>window.location('home.php')</script>";
 }else{
     header('Location : index.php');
 }
