e82bdbbdec2e4c4783d386c1d770a96f5e74996f
mishraCs <aakashmishrabba@gmail.com>
2024-07-09 20:23:29 +0530

    Today I'm here added a amazing footer, it's setup complete

--- helper/footer.php
+++ helper/footer.php
@@ -1,4 +1,18 @@
+<link rel="stylesheet" href="./css/footer.css">
+<?php
+$sql = "SELECT * FROM users ORDER BY user_id DESC LIMIT 1";
+$result = $conn->query($sql);
+if($result->num_rows > 0){
+    $row = mysqli_fetch_assoc($result);
+    $userId = $row['user_id'];
+}else{
+    die('connection errror');
+}
+?>
+<?php include './2023-footer/footer/dist/index.php'?>
 </div>
 <script src="js/script.js" defer></script>
 </body>
 </html>
+
+
