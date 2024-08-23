f96116f02b3522b499586d3f539a3d73cb8829ff
mishraCs <aakashmishrabba@gmail.com>
2024-08-23 18:42:38 +0530

    The css addition completed

--- index.php
+++ index.php
@@ -1,40 +1,31 @@
-<?php include 'HtDocs/Views/Frontend/Header.php';?>
-<link rel="stylesheet" href="./css/index.css">
+<?php include 'HtDocs/Views/Frontend/Header.php'; ?>
 <div class="about_div">
   <img class="banner_img" src="File/cloud.jpeg">
   <div class="img_text">
     <a href="index.php"><p>Specific Image</p></a>
   </div>
 </div>
-<h1>Customers like these <b><i>category</i></b></h1>
+<h1>Customers like these <b><i>categories</i></b></h1>
 <?php 
 try {
-    $latestCategories = $LatestCategory->LatestCategory();
-    foreach ($latestCategories as $category) {
-        $categoryName = $category['latest_search_category'];
-        $latestCategoryInfo = $CategoryObj->selectCategoryByName($categoryName);
-            foreach($latestCategoryInfo as $Category) {
-                $categoryId = $Category['category_id']; ?>
-                <div class="col-md-3 blur-div">
-                    <div class="img_user_info">
-                        <img 
-                            class="categorry_view_img" 
-                            id="scroll-up" 
-                            onclick="viewSubCategory('<?php echo $Category['category_id']; ?>')" 
-                            src="<?php echo $Category['category_image']; ?>" 
-                            alt="Category Image"
-                        >
-                        <input id="categoryId" type="hidden" value="<?php echo $Category['category_id']; ?>">
-                    </div>
-                </div>
-                <?php
+  $latestCategories = $LatestCategory->LatestCategory();?> 
+  <div class="row">
+    <?php foreach ($latestCategories as $category) {
+            $categoryName = $category['latest_search_category'];
+            $latestCategoryInfo = $CategoryObj->selectCategoryByName($categoryName);
+            foreach ($latestCategoryInfo as $Category) {?>
+                <div class="img_user_info">
+                    <img class="categorry_view_img" id="scroll-up" onclick="viewSubCategory('<?php echo $Category['category_id']; ?>')" src="<?php echo $Category['category_image']; ?>" alt="Category Image">
+                    <input type="hidden" value="<?php echo $Category['category_id']; ?>">
+                </div><?php
             }
-    }
+          }?> 
+  </div><?php
 } catch (\Throwable $th) {
-    echo "Error: " . $th->getMessage();
+  echo "Error: " . $th->getMessage();
 }
 ?>
-<div id="viewSubCategory" class="sub-cat-view"></div>
+<div id="viewSubCategory" name="prdtContain" class="row sub-cat-view"></div>
 <!-- Modal add-pro-cookie -->
 <div class="modal fade cart-add-product add-cart-modal" id="successModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
@@ -60,3 +51,4 @@ try {
 </div>
 <!-- Modal already-add -->
 <?php include 'HtDocs/Views/Frontend/Footer.php'; ?>
+
